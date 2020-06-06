cdac025e9902e544d03d135bd4eaa35952d26fd7
Robin <robinsinghmahrok@gmail.com>
2020-06-07 03:02:21 +0530

    callback function

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var rect={
-    perimeter:(x,y)=>(2*(x+y)),
-    area: (x,y)=>(x*y)
-};
+var rect=require('./rectangle');
 
 function solveRect(l,b)
 {
     console.log("Solving for rectangle with l="+l+" and b="+b);
-    if(l<=0|| b<=0)
-    {
-        console.log("Inappropriate dimensions");
-    }
-    else{
-        console.log("area is:"+rect.area(l,b)+" and perimeter is:"+rect.perimeter(l,b));
-
-    }
+  
+    rect(l,b,(err,rectangle) =>{
+        if(err)
+        {
+            console.log("Error "+err.message);
+        }
+        else{
+            console.log("For rectangle with l="+l+" and b="+b+" area is: "+rectangle.area()+" and perimeter is: "+rectangle.perimeter());
+            
+        }
+    });
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2,4);
