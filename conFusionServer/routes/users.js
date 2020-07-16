2b1bb3a1d8f9b9d0f13f93728541e1b5588470a1
Robin <robinsinghmahrok@gmail.com>
2020-07-16 23:18:10 +0530

    3rd assignment

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -9,8 +9,14 @@ var authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+  User.find({})
+  .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
@@ -22,11 +28,24 @@ router.post('/signup', (req, res, next) => {
     res.json({err:err});
     }
     else {
+      if (req.body.firstname)
+      user.firstname = req.body.firstname;
+    if (req.body.lastname)
+      user.lastname = req.body.lastname;
+    user.save((err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+        return ;
+      }
+    
      passport.authenticate('local')(req,res,()=>{
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
       res.json({success: true, status: 'Registration Successful!'});
      });
+    });
     }
   });
 });
